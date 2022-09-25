4d172197851b316ace929e6fb7d94f35b626828b
Hrushikesh-zade <88075635+Hrushikesh-zade@users.noreply.github.com>
2022-09-25 14:54:14 +0530

    up

--- BookApp-Backend/book-service/src/main/java/in/bookapp/bookservice/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package in.bookapp.bookservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import in.bookapp.bookservice.model.Book;
-
-public interface BookRepository extends MongoRepository<Book, String> {
-    
-}
